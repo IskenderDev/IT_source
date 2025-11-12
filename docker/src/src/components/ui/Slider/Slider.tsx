16a771f0850057750b3f97e815568149a5c17426
NurFullS <nurzhenishov10@gmail.com>
2025-11-13 00:01:53 +0600

    fix style

--- docker/src/src/components/ui/Slider/Slider.tsx
+++ docker/src/src/components/ui/Slider/Slider.tsx
@@ -55,7 +55,7 @@ export default function Slider({
   mobileSlideMinH = 560,
   imageMaxHMobile = 192,
   imageMaxHDesktop = 480,
-  imageRightPx = 112,
+  imageRightPx = 40,
 }: Props) {
   const emblaPlugins = useMemo(
     () =>
@@ -125,7 +125,7 @@ export default function Slider({
                   className="
                     relative w-full
                     min-h-[var(--mh)] md:min-h-0
-                    h-auto md:h-[520px] lg:h-[560px]
+                    h-auto md:h-200 lg:h-[560px]
                     rounded-[24px] md:rounded-[28px]
                     p-4 md:p-8 lg:p-9 text-white
                     shadow-[0_8px_40px_rgba(0,0,0,0.35)]
@@ -208,7 +208,7 @@ export default function Slider({
                       <h3 className="text-[32px] lg:text-[40px] font-bold leading-snug">
                         {s.title}
                       </h3>
-                      <p className="mt-6 text-[16px] leading-relaxed text-white/90 whitespace-pre-line">
+                      <p className="mt-6 text-[16px] w-70 leading-relaxed text-white/90 whitespace-pre-line">
                         {s.text}
                       </p>
 
@@ -270,6 +270,7 @@ export default function Slider({
                           className="
                             absolute -translate-y-1/2
                             object-contain
+                            w-50
                             drop-shadow-[0_20px_40px_rgba(0,0,0,0.35)]
                           "
                         />
---
+++ docker/src/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'mobile-lg': {'max': '768px'}, // до 768px включительно
+      },
+    },
+  },
+}
